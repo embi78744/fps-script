f7c7e54cd02cb7ca293560c3722cb047d4c8c7a1
embi78744 <embi78744@gmail.com>
2026-04-09 22:05:23 +0700

    1

--- script.lua
+++ script.lua
@@ -2,7 +2,6 @@ local g=Instance.new("ScreenGui",game.CoreGui)
 local f=Instance.new("Frame",g)
 local t=Instance.new("TextLabel",f)
 
--- UI to hơn
 f.Position=UDim2.new(0.5,-180,0,10)
 f.Size=UDim2.new(0,360,0,100)
 f.BackgroundColor3=Color3.fromRGB(0,0,0)
@@ -20,24 +19,24 @@ t.TextScaled=true
 -- FPS mượt
 local fps=0
 local frames=0
-local last=tick()
 
 game:GetService("RunService").RenderStepped:Connect(function()
 	frames += 1
-	if tick()-last >= 1 then
-		fps=frames
-		frames=0
-		last=tick()
-	end
+end)
 
-	-- ping ổn định
-	local ping=math.random(60,120)
+-- update chậm lại (mượt)
+while true do
+	task.wait(0.5)
 
-	-- đổi màu theo ping
+	fps = frames * 2 -- vì 0.5s
+	frames = 0
+
+	local ping = math.random(60,120)
+
 	local color=Color3.fromRGB(0,255,0)
 	if ping>100 then color=Color3.fromRGB(255,170,0) end
 	if ping>150 then color=Color3.fromRGB(255,0,0) end
 
 	t.TextColor3=color
-	t.Text="FPS: "..fps.." | Ping: "..ping.." ms\n\n discord.gg/yourlink"
-end)
+	t.Text="FPS: "..fps.." | Ping: "..ping.." ms\n\ndiscord.gg/yourlink"
+end
